5618964ad2137c1a60cab548455c72332f41c392
Ryan900725 <ryanwhite11391@gmail.com>
2025-03-10 14:17:13 -0400

    second

--- src/CustomizedAccordian.js
+++ src/CustomizedAccordian.js
@@ -17,7 +17,6 @@ export default function CustomizedAccordions() {
   }, []);
 
   const handleSearchText = (e) => {
-    console.log(searchText);
     setSearchText(e.target.value);
   };
   const handleSummaryAdd = (e) => {
@@ -26,22 +25,50 @@ export default function CustomizedAccordions() {
   const handleDetailsAdd = (e) => {
     setDetailsAdd(e.target.value);
   };
-  const addItem = async () => {
-    try {
-      await axios.put("http://localhost:5000/add-data", {
-        summary: summaryAdd,
-        details: detailsAdd,
-      });
-      alert("Data updated successfully");
-    } catch (error) {
-      alert("Error updating data");
+
+  const isSimilar = (first, second) => {
+    let tmp;
+    let row = [];
+    // Initialize the row
+    for(let i = 0; i < first.length; i++) {
+        row[i] = i;
     }
-  };
+    for(let i = 1; i <= second.length; i++) {
+        tmp = i;
+        for(let j = 1; j <= first.length; j++) {
+            let cost = first[j-1] === second[i-1] ? 0 : 1;
+            let val = Math.min(row[j-1] + cost, Math.min(tmp + 1, row[j] + 1));
+            row[j-1] = tmp;
+            tmp = val;
+        }
+    }
+    return row[first.length-1];
+  }
+  const isSimilarWordContained = (keywords, txt) => {
+    let txtArr = txt.split(" ");
+    for (let i = 0; i < keywords.length; i ++) {
+      for (let j = 0; j < txtArr.length; j ++) {
+        let r = isSimilar(keywords[i], txtArr[j]);
+        if (r <= keywords[i].length/5*2) return true;
+      }
+    }
+  }
+  // const addItem = async () => {
+  //   try {
+  //     await axios.put("http://localhost:5000/add-data", {
+  //       summary: summaryAdd,
+  //       details: detailsAdd,
+  //     });
+  //     alert("Data updated successfully");
+  //   } catch (error) {
+  //     alert("Error updating data");
+  //   }
+  // };
 
   return (
     <div>
       <>
-        <div>
+        {/* <div>
           <input
             type="text"
             id="summary-add"
@@ -58,15 +85,15 @@ export default function CustomizedAccordions() {
             style={{ width: "50%", height: "300px", padding: "5px" }}
             onChange={handleDetailsAdd}
           />
-        </div>
-        <div>
+        </div> */}
+        {/* <div>
           <input
             type="button"
             onClick={addItem}
             style={{ width: "30%", padding: "5px" }}
             value="AddItem"
           />
-        </div>
+        </div> */}
       </>
 
       <input
@@ -77,8 +104,11 @@ export default function CustomizedAccordions() {
       />
       {jsonData.map((d, i) => {
         return (
-          (d.summary.toLowerCase().includes(searchText.toLowerCase()) ||
-            d.details.toLowerCase().includes(searchText.toLowerCase())) && (
+          (
+            d.summary.toLowerCase().includes(searchText.toLowerCase()) ||
+            // d.details.toLowerCase().includes(searchText.toLowerCase()) ||
+            isSimilarWordContained(d.keywords, searchText)
+          ) && (
             <AccordionItem
               key={i}
               panelName={"panel" + (i + 1)}
